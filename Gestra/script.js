c5b937996f490ff071d16f9d8ce355b376d2dabd
Alyaras <alyarahmadayanis@gmail.com>
2025-10-26 23:08:51 +0700

    text to speech

--- Gestra/script.js
+++ Gestra/script.js
@@ -42,3 +42,6 @@ window.onmousemove = e => handleOnMove(e);
 
 window.ontouchmove = e => handleOnMove(e.touches[0]);
 
+document.getElementById("tts-btn").addEventListener("click", function() {
+    window.location.href = "speechToText.html";
+});
