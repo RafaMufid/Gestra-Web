98c1b0e326d16e6ecb1899ef8d44156e5f590087
Yappier Albertus <yappier.albert1@gmail.com>
2025-12-08 11:09:49 +0700

    backend
    laravel backend hore hore

---
+++ Gestra BackEnd/Gestra/app/Models/UserData.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+
+class UserData extends Model
+{
+
+    use HasApiTokens;
+
+    protected $table = 'user_data';
+
+    protected $fillable = [
+        'username',
+        'email',
+        'password',
+        'user_type',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+}
