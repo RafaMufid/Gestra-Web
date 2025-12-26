289d4d6c7df802d5d96dfd32670e5ad1273a73de
RafaMufid <akurafamufid@gmail.com>
2025-12-26 20:26:56 +0700

    menambahkan mcr untuk menyimpan hasil translasi

---
+++ Gestra BackEnd/Gestra/app/Models/LearningHistory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LearningHistory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'gesture_name',
+        'accuracy',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(UserData::class, 'user_id');
+    }
+}
